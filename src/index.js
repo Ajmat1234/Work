13700ee898052ef66a1b8c537f9060c3ff46b966
Ajmat <hunterboyz73770@gmail.com>
2025-05-10 21:43:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // Agar yeh file hai toh, nahi toh ignore kar dena
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
